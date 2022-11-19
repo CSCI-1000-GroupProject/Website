662efba010bdc0c7390aab535224176608979ffd
Ethan Epperson <etep9497@colorado.edu>
2022-11-18 22:54:47 -0700

    adding to index and about me

--- src/pages/ethan.js
+++ src/pages/ethan.js
@@ -1,183 +1,189 @@
 import Head from 'next/head'
-
+ 
 export default function Home() {
-  return (
-    <div className='container'>
-      <Head>
-        <title>About Me</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <main>
-        <h1 className='title'>Hello, I'm Ethan Epperson!</h1>
-
-        <p className='description'>
-          I'm a first year computer science student in the BA program. I enjoy listening to music. I don't have a favorite song right now but  
-          but I enjoy rap, shoegaze, alternative, jazz, etc. My favorite show of all time is Cowboy Bebop. I like to exercise as well.
-          I like film, animation, fashion, cars, and many other things.
-        </p>
-
-        <a className='button' href='/'>
-            <p> Back to Home Page</p>
-          </a>
-      </main>
-
-      <footer>
-        <a href='' target='_blank' rel='noopener noreferrer'>
-          Copyright CSCI 1000
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        .description2 {
-          line-height: 1.5;
-          font-size: 1.25rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+ return (
+   <div className='container'>
+     <Head>
+       <title>About Me</title>
+       <link rel='icon' href='/favicon.ico' />
+     </Head>
+ 
+     <main>
+       <h1 className='title'>Hello, I'm Ethan Epperson!</h1>
+ 
+       {/* <img
+           className='pic'
+           src='https://pbs.twimg.com/profile_images/415961110635110400/xPO9YqTC.jpeg'
+           alt='ron burgundy'
+         /> */}
+       <p className='description'>
+         I'm a first year computer science student in the BA program. I enjoy listening to music. I don't have a favorite song right now but 
+         but I enjoy rap, shoegaze, alternative, jazz, etc. My favorite show of all time is Cowboy Bebop. I like to exercise as well.
+         I like film, animation, fashion, cars, and many other things.
+       </p>
+ 
+       <a className='button' href='/'>
+           <p> Back to Home Page</p>
+         </a>
+     </main>
+ 
+     <footer>
+       <a href='' target='_blank' rel='noopener noreferrer'>
+         Copyright CSCI 1000
+       </a>
+     </footer>
+ 
+     <style jsx>{`
+       .container {
+         min-height: 100vh;
+         padding: 0 0.5rem;
+         display: flex;
+         flex-direction: column;
+         justify-content: center;
+         align-items: center;
+       }
+ 
+       main {
+         padding: 5rem 0;
+         flex: 1;
+         display: flex;
+         flex-direction: column;
+         justify-content: center;
+         align-items: center;
+       }
+ 
+       footer {
+         width: 100%;
+         height: 100px;
+         border-top: 1px solid #eaeaea;
+         display: flex;
+         justify-content: center;
+         align-items: center;
+       }
+ 
+       footer img {
+         margin-left: 0.5rem;
+       }
+ 
+       footer a {
+         display: flex;
+         justify-content: center;
+         align-items: center;
+       }
+ 
+       a {
+         color: inherit;
+         text-decoration: none;
+       }
+ 
+       .title a {
+         color: #0070f3;
+         text-decoration: none;
+       }
+ 
+       .title a:hover,
+       .title a:focus,
+       .title a:active {
+         text-decoration: underline;
+       }
+ 
+       .title {
+         margin: 0;
+         line-height: 1.15;
+         font-size: 4rem;
+       }
+ 
+       .title,
+       .description {
+         text-align: center;
+       }
+ 
+       .description {
+         line-height: 1.5;
+         font-size: 1.5rem;
+       }
+ 
+       .description2 {
+         line-height: 1.5;
+         font-size: 1.25rem;
+       }
+ 
+       code {
+         background: #fafafa;
+         border-radius: 5px;
+         padding: 0.75rem;
+         font-size: 1.1rem;
+         font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+           DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+       }
+ 
+       .grid {
+         display: flex;
+         align-items: center;
+         justify-content: center;
+         flex-wrap: wrap;
+ 
+         max-width: 800px;
+         margin-top: 3rem;
+       }
+ 
+       .card {
+         margin: 1rem;
+         flex-basis: 45%;
+         padding: 1.5rem;
+         text-align: left;
+         color: inherit;
+         text-decoration: none;
+         border: 1px solid #eaeaea;
+         border-radius: 10px;
+         transition: color 0.15s ease, border-color 0.15s ease;
+       }
+ 
+       .card:hover,
+       .card:focus,
+       .card:active {
+         color: #0070f3;
+         border-color: #0070f3;
+       }
+ 
+       .card h3 {
+         margin: 0 0 1rem 0;
+         font-size: 1.5rem;
+       }
+ 
+       .card p {
+         margin: 0;
+         font-size: 1.25rem;
+         line-height: 1.5;
+       }
+ 
+       .logo {
+         height: 1em;
+       }
+ 
+       @media (max-width: 600px) {
+         .grid {
+           width: 100%;
+           flex-direction: column;
+         }
+       }
+     `}</style>
+ 
+     <style jsx global>{`
+       html,
+       body {
+         padding: 0;
+         margin: 0;
+         font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+           Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+           sans-serif;
+       }
+ 
+       * {
+         box-sizing: border-box;
+       }
+     `}</style>
+   </div>
+ );
 }
+ 
